c9210cee0ff0e43592bb54411c0e409f92c2428f
ryandaepark <ryanp0209@gmail.com>
2023-11-29 14:22:23 -0800

    Add post complete

--- api/index.js
+++ api/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const Post = require('./models/Post')
 var bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const { flushSync } = require('react-dom');
+const uploadMiddleware = multer({ dest: 'uploads/' })
+const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'aiqouwbe7823742b3dq9wnd10nd98wbdnomx';
@@ -58,6 +63,25 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
-})
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    //renaming file to have extension
+    const {originalname, path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+    const {title, summary, content} = req.body;
+    const postDoc = await Post.create({
+        title, 
+        summary, 
+        content, 
+        cover: newPath,
+    });
+
+    res.json(postDoc);
+});
 
 app.listen(4000); 
